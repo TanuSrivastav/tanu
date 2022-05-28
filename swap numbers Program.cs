3bb88b8854bf130600cd32abbeb3d4e4d1067d0b
Tanu Srivastav <tanusrivastava031@gmail.com>
2022-05-28 10:31:30 +0530

    first commit

---
+++ swap numbers Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void swap(ref int x, ref int y)
+        {
+            int tempswap = x;
+            x = y;
+            y = tempswap;
+        }
+        static void Main(string[] args)
+        {
+            int a = 5, b = 6;
+            Console.WriteLine("a =" + " " + a);
+            Console.WriteLine("b=" + " " + b);
+            swap(ref a, ref b);
+            Console.WriteLine();
+            Console.WriteLine("Value of a, b after swapping");
+            Console.WriteLine();
+            Console.WriteLine("a=" + " " + a);
+            Console.WriteLine("b=" + " " +b);
+            Console.ReadLine();
+        
+
+
+ 
+                           
+               
+
+        }
+    }
+}
