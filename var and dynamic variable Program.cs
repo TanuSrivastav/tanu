5e2c861098914cf7b005a239e9730a7a28985b77
Tanu Srivastav <tanusrivastava031@gmail.com>
2022-05-28 13:22:43 +0530

    first commit

---
+++ var and dynamic variable Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace program
+{ 
+class Class4
+{
+    static void Main(string[] args)
+        {  
+             var x = 1;
+             var y = "Tanu";
+          //y = 1;
+             dynamic x1= 2;
+              x1 = "Tanu";
+Console.WriteLine(x);
+Console.WriteLine(x1);
+        }
+}
+}
